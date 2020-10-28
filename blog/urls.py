41068f3551cb0339e97590a2f0b6168c70f00a73
PKSingh0017 <priyanshusingh1998@gmail.com>
2020-10-28 09:19:14 +0530

    Added functionality to update and delete posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,18 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView
+)
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),            # Empty path means the home page
+    path('', PostListView.as_view(), name='blog-home'),   # Empty path means the home page
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
--- users/views.py
+++ users/views.py
@@ -15,7 +15,7 @@ def register(request):
     if request.method == 'POST':
         form = UserRegisterForm(request.POST)
         if form.is_valid():
-            form.save()
+            # form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Your acccount has been created! You can now log in!')
             return redirect('login')
